7b12b9119d39941ead02406b52b90e883026546b
vyazemskaya <vyazemskayanna@gmail.com>
2025-02-07 17:47:32 +0300

    2.0.0

--- src/js/anim/parallax.js
+++ src/js/anim/parallax.js
@@ -1,15 +1,15 @@
 import gsap from 'gsap';
 import { removeClasses } from '../utils/utils';
+import { ScrollTrigger } from 'gsap/all';
 
 document.addEventListener('DOMContentLoaded', function () {
   const sections = gsap.utils.toArray('[data-section]');
 
   if (sections) {
-    sections.forEach(section => {
+    sections.forEach((section, i) => {
       const addClass = e => {
-        const el = document.querySelector(
-          `.nav [data-anchor=".${e.vars.trigger.dataset.section}"]`
-        );
+        const d = e.dataset.section;
+        const el = document.querySelector(`.nav [data-anchor=".${d}"]`);
         removeClasses(document.querySelectorAll('.nav__item'), '_is-active');
         el.parentElement.classList.add('_is-active');
       };
@@ -19,14 +19,22 @@ document.addEventListener('DOMContentLoaded', function () {
           ease: 'none',
           scrollTrigger: {
             trigger: section,
-            start: 'top top',
-            end: '+=260rem top',
+            // start: 'top bottom',
+            start: 'top center',
             scrub: true,
             onEnter: e => {
-              addClass(e);
+              addClass(e.vars.trigger);
             },
-            onEnterBack: e => {
-              addClass(e);
+            onLeaveBack: e => {
+              const el = i === 0 ? section : sections[i - 1];
+
+              removeClasses(
+                document.querySelectorAll('.nav__item'),
+                '_is-active'
+              );
+              document
+                .querySelector(`.nav [data-anchor=".${el.dataset.section}"]`)
+                .parentElement.classList.add('_is-active');
             },
           },
         })
